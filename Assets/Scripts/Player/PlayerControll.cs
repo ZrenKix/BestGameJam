1ed4005ae7763c111e7f0ddeda314e66d3a00321
linpack12 <55255203+linpack12@users.noreply.github.com>
2023-06-05 14:17:51 +0200

    kamera + pick up funktionlitet plus gubbens rotation

--- Assets/Scripts/Player/PlayerControll.cs
+++ Assets/Scripts/Player/PlayerControll.cs
@@ -6,10 +6,13 @@ public class PlayerControll : MonoBehaviour
     public float sprintSpeed = 10f;
     public float jumpForce = 5f;
     public float groundRaycastAvstånd = 0.1f;
+    public float pickupRaycastDistance = 2f;
 
     private bool springer = false;
     private bool hoppar = false;
     private bool ärPåMark = false;
+    private bool isCarrying = false;
+    private GameObject carriedObject;
 
     private Rigidbody rb;
 
@@ -24,6 +27,12 @@ public class PlayerControll : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        if (movement.magnitude > 0.1f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
+        }
 
         if (springer)
         {
@@ -52,6 +61,25 @@ public class PlayerControll : MonoBehaviour
         {
             springer = false;
         }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (!isCarrying)
+            {
+                PickUpObject();
+                Debug.Log("Pick uped object");
+            }
+            else
+            {
+                DropObject();
+                Debug.Log("yeeet");
+            }
+        }
+        if (isCarrying)
+        {
+            CarryObject();
+            Debug.Log("jag bär på något");
+        }
     }
 
     void FixedUpdate()
@@ -69,9 +97,43 @@ public class PlayerControll : MonoBehaviour
         }
     }
 
+    void PickUpObject()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, pickupRaycastDistance))
+        {
+            if (hit.collider.CompareTag("PickUp"))
+            {
+                carriedObject = hit.collider.gameObject;
+                carriedObject.GetComponent<Rigidbody>().isKinematic = true;
+                carriedObject.transform.SetParent(transform);
+                carriedObject.transform.localPosition = new Vector3(0f, 0.5f, 1f);
+                isCarrying = true;
+            }
+        }
+    }
+
+    void CarryObject()
+    {
+        carriedObject.transform.position = transform.position + transform.forward * 1.5f;
+        carriedObject.GetComponent<Rigidbody>().velocity = rb.velocity;
+    }
+
+    void DropObject()
+    {
+        carriedObject.transform.SetParent(null);
+        carriedObject.GetComponent<Rigidbody>().isKinematic = false;
+        carriedObject = null;
+        isCarrying = false;
+    }
+
     void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundRaycastAvstånd);
+
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * pickupRaycastDistance);
     }
 }
---
+++ Assets/ThirdPersonCamera.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public Transform target;     // The target to follow
+    public Vector3 offset = new Vector3(0f, 2f, -5f);   // Camera offset from the target
+    public float rotationSpeed = 5f;    // Speed at which the camera rotates
+
+    private void LateUpdate()
+    {
+        // Calculate the desired position of the camera based on the target and offset
+        Vector3 desiredPosition = target.position + offset;
+
+        // Smoothly move the camera towards the desired position
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, rotationSpeed * Time.deltaTime);
+
+        // Calculate the desired rotation of the camera to look at the target
+        Quaternion desiredRotation = Quaternion.LookRotation(target.position - transform.position);
+
+        // Smoothly rotate the camera towards the desired rotation
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
+    }
+}
