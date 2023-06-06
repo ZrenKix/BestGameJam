7cda1a170eeb5604f0457d30881c5227044ffff2
linpack12 <55255203+linpack12@users.noreply.github.com>
2023-06-06 11:56:46 +0200

    link utan bugg när han tart upp saker

--- Assets/Scripts/Player/PlayerControll.cs
+++ Assets/Scripts/Player/PlayerControll.cs
@@ -145,20 +145,22 @@ public class PlayerControll : MonoBehaviour
                
                 int playerLayer = LayerMask.NameToLayer("Player");
                 Physics.IgnoreLayerCollision(playerLayer, carriedObjectLayer, true);
-
+                Debug.Log(hit + "kolliderar med pickup");
                 carriedObject.GetComponent<Rigidbody>().isKinematic = true;
                 carriedObject.transform.SetParent(transform);
                 carriedObject.transform.localPosition = new Vector3(0f, 0.5f, 0.5f);
                 isCarrying = true;
+                animator.SetBool("Grabbing", true);
             }
         }
     }
 
     void CarryObject()
     {
-        Vector3 desiredPosition = transform.position + transform.forward * 1f + Vector3.up * 0.5f;
+        Vector3 desiredPosition = transform.position + transform.forward * 0.5f + Vector3.up * 0.5f;
         carriedObject.transform.position = desiredPosition;
         carriedObject.GetComponent<Rigidbody>().velocity = rb.velocity;
+
     }
 
     void DropObject()
@@ -166,7 +168,7 @@ public class PlayerControll : MonoBehaviour
         int playerLayer = LayerMask.NameToLayer("Player");
         int carriedObjectLayer = LayerMask.NameToLayer("CarriedObject");
         Physics.IgnoreLayerCollision(playerLayer, carriedObjectLayer, false);
-
+        
         carriedObject.transform.SetParent(null);
         carriedObject.GetComponent<Rigidbody>().isKinematic = false;
         carriedObject.layer = defaultLayer;
@@ -180,6 +182,7 @@ public class PlayerControll : MonoBehaviour
         carriedObjectRb.angularVelocity = new Vector3(0f, 2f, 0f); 
         carriedObject = null;
         isCarrying = false;
+        animator.SetBool("Grabbing", false);
     }
 
     void CreateDust()
