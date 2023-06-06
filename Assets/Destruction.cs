2db9f9dfb77568c2e14f272a059faa9259f5293b
linpack12 <55255203+linpack12@users.noreply.github.com>
2023-06-06 19:10:42 +0200

    cinamachine

--- Assets/Destruction.cs
+++ Assets/Destruction.cs
@@ -17,5 +17,34 @@ public class Destruction : MonoBehaviour
         cubeWidth = transform.localScale.z;
         cubeHeight = transform.localScale.y;
         cubeDepth = transform.localScale.x;
+
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        mesh.gameObject.GetComponent<Transform>().localScale = new Vector3(cubeScale, cubeScale, cubeScale);
+
+    }
+
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        if(collision.gameObject.tag == "Sword")
+        {
+            CreateCube();
+        }
+    }
+
+    void CreateCube()
+    {
+        for (float x = 0; x < cubeWidth; x += cubeScale)
+        {
+            for (float y = 0; y < cubeHeight; y += cubeScale)
+            {
+                for (float z = 0; z < cubeDepth; z+= cubeScale)
+                {
+                    Vector3 vec = transform.position;
+
+                    GameObject cubes = (GameObject)Instantiate(mesh, vec + new Vector3(x, y, z), Quaternion.identity);
+                    cubes.gameObject.GetComponent<MeshRenderer>().material = gameObject.GetComponent<MeshRenderer>().material;
+                }
+            }
+        }
     }
 }
