fc3221cef2ff08bd53524df6d48cb9171bbe7e65
ZrenKix <50847546+ZrenKix@users.noreply.github.com>
2023-06-07 11:47:48 +0200

    Changes

--- Assets/Scripts/Player/HealthBar.cs
+++ Assets/Scripts/Player/HealthBar.cs
@@ -8,10 +8,23 @@ public class HealthBar : MonoBehaviour
 
     private void Update()
     {
-        for (int i = 0; i < health.getCurrenthealth(); i++)
+        if (health.getCurrenthealth() == 0)
         {
-            hearts[i].SetActive(false);
-            hearts[-i].SetActive(true);
+            hearts[0].SetActive(true);
+            hearts[1].SetActive(false);
+            hearts[2].SetActive(false);
+        }
+        else if (health.getCurrenthealth() == 1)
+        {
+            hearts[0].SetActive(true);
+            hearts[1].SetActive(true);
+            hearts[2].SetActive(false);
+        }
+        else if (health.getCurrenthealth() >= 2)
+        {
+            hearts[0].SetActive(true);
+            hearts[1].SetActive(true);
+            hearts[2].SetActive(true);
         }
     }
 }
