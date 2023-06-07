b04cad596638a4d522131d75ecb810a27a0424af
linpack12 <55255203+linpack12@users.noreply.github.com>
2023-06-07 12:32:48 +0200

    spawning chickens

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject enemyChicken;
+    public Transform link;
 
-    // Update is called once per frame
+    [SerializeField]
+    private float timer = 5f;
+    private float timeElapsed = 0f;
+
+    public bool chickenSpawning = false;
+
+
     void Update()
     {
         
+        if (chickenSpawning && timer < timeElapsed)
+        {
+            Vector3 spawningPosition = link.position + new Vector3(Random.Range(-10f, 10f), 10f, Random.Range(-10f, 10f));
+            Instantiate(enemyChicken, spawningPosition, new Quaternion(0f, 0f, 0f, 0f));
+            timeElapsed = 0f;
+        }
+        timeElapsed += Time.deltaTime;
     }
 }
