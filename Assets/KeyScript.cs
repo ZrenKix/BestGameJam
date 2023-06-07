ae79fcf1e4620fc42306eb4a13eb4b54495b6927
linpack12 <55255203+linpack12@users.noreply.github.com>
2023-06-07 16:20:26 +0200

    Revert "Revert "key skript funkar""
    This reverts commit f05a57f0b7046aabf82f0e08c134f70cc7f03878.

--- Assets/KeyScript.cs
+++ Assets/KeyScript.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class KeyScript : MonoBehaviour
 {
+    [SerializeField]
     GameObject burDörr;
+    [SerializeField]
     GameObject vakter;
 
     private void OnCollisionEnter(Collision collision) {if(collision.gameObject == burDörr && vakter == null) burDörr.SetActive(false);}
