8f210d084c05765aa2d967ab105dc541f2d7368b
Aletsux <71812424+Aletsux@users.noreply.github.com>
2023-06-08 17:52:22 +0200

    New: Player Hit effect

--- Assets/Scripts/Player/HealthBar.cs
+++ Assets/Scripts/Player/HealthBar.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private GameObject[] hearts;
+    ParticleSystem blood;
+
 
     public Health health;
-
+    bool takeDamage = false; 
+    int savedHealth = 0;
+    
+    private void Start() {
+        blood = GetComponentInParent<ParticleSystem>();
+        savedHealth = health.getCurrentHealth();
+        blood.Stop();
+    }
     private void Update()
     {
         if (health.getCurrentHealth() == 0)
@@ -13,6 +22,7 @@ public class HealthBar : MonoBehaviour
             hearts[0].SetActive(true);
             hearts[1].SetActive(false);
             hearts[2].SetActive(false);
+            
         }
         else if (health.getCurrentHealth() == 1)
         {
@@ -25,6 +35,19 @@ public class HealthBar : MonoBehaviour
             hearts[0].SetActive(true);
             hearts[1].SetActive(true);
             hearts[2].SetActive(true);
+        }
+
+        if(!takeDamage && savedHealth != health.getCurrentHealth()) {
+            takeDamage = true;
+            PlayBloodSplatter();
+        }
+    }
+
+    private void PlayBloodSplatter() {
+        if(takeDamage) {
+            blood.Play();
+            takeDamage = false;
+            savedHealth = health.getCurrentHealth();
         }
     }
 }
