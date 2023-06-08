e383c2807f250cf98668deaee090828bbf06cd87
ZrenKix <50847546+ZrenKix@users.noreply.github.com>
2023-06-08 12:05:39 +0200

    Changes

--- Assets/Scripts/SceneObjects/WorldSpawner.cs
+++ Assets/Scripts/SceneObjects/WorldSpawner.cs
@@ -1,16 +1,42 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class WorldSpawner : MonoBehaviour
 {
     [SerializeField] private Item[] items;
     [SerializeField] private Terrain terrain;
 
-    private void FixedUpdate()
+    // Terrain values
+    private int terrainWidth; // terrain size (x)
+    private int terrainLength; // terrain size (z)
+    private int terrainPosX; // terrain position x
+    private int terrainPosZ; // terrain position z
+
+    void Start()
     {
-        
+        terrainWidth = (int)terrain.terrainData.size.x;
+        terrainLength = (int)terrain.terrainData.size.z;
+        terrainPosX = (int)terrain.transform.position.x;
+        terrainPosZ = (int)terrain.transform.position.z;
+    }
+
+    void Update()
+    {
+        foreach (Item item in items)
+        {
+            if (item.currentAmount <= item.maxAmount)
+            {
+                item.increaseAmount();
+                int posx = Random.Range(terrainPosX, terrainPosX + terrainWidth);
+                int posz = Random.Range(terrainPosZ, terrainPosZ + terrainLength);
+                float posy = Terrain.activeTerrain.SampleHeight(new Vector3(posx, 0, posz));
+                Instantiate(item.gameObject, new Vector3(posx, posy, posz), Quaternion.identity);
+            } else
+            {
+                Debug.Log("Generate objects complete!");
+            }
+        }
     }
 }
 
@@ -18,5 +44,11 @@ public class WorldSpawner : MonoBehaviour
 public struct Item
 {
     public GameObject gameObject;
-    public float spawnRate;
+    public int maxAmount;
+    public int currentAmount;
+
+    public void increaseAmount()
+    {
+        currentAmount++;
+    }
 }
