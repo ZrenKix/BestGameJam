d4e177aba1cee8f9bcd555e10fe6695e46047001
ZrenKix <50847546+ZrenKix@users.noreply.github.com>
2023-06-08 11:42:18 +0200

    Fixed item

--- Assets/Scripts/SceneObjects/WorldSpawner.cs
+++ Assets/Scripts/SceneObjects/WorldSpawner.cs
@@ -5,25 +5,18 @@ using UnityEngine;
 
 public class WorldSpawner : MonoBehaviour
 {
-    [SerializeField] private Item items;
+    [SerializeField] private Item[] items;
     [SerializeField] private Terrain terrain;
 
     private void FixedUpdate()
     {
         
     }
-
 }
 
-
-
-public class ItemObject : MonoBehaviour {
-    
-
-    [Serializable]
-    public struct Item
-    {
-        private GameObject gameObject;
-        private float spawnRate;
-    }
+[Serializable]
+public struct Item
+{
+    public GameObject gameObject;
+    public float spawnRate;
 }
